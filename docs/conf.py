0d6f7ab0c8e2b29408521034eded156ee1da9fc7
Vinicius Mendes <vinicius.mendes@loadsmart.com>
2021-10-07 22:21:37 -0300

    Apply styles and customize the profile slide.

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,15 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'A importancia da documentacao no trabalho distribuido'
-copyright = '2021, Vinicius Mendes'
-author = 'Vinicius Mendes'
+project = "A importancia da documentacao no trabalho distribuido"
+copyright = "2021, Vinicius Mendes"
+author = "Vinicius Mendes"
 
 # The short X.Y version
-version = '2021.10.04'
+version = "2021.10.04"
 
 # The full version, including alpha/beta/rc tags
-release = '2021.10.04'
+release = "2021.10.04"
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,16 +34,16 @@ release = '2021.10.04'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'hieroglyph',
+    "hieroglyph",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,12 +51,26 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+
+# Options for Hieroglyph
+slide_theme = "vbmendes_theme"
+slide_theme_path = ["_themes"]
+slide_theme_options = {
+    "presenters": [
+        {
+            "name": "Vinicius Mendes",
+            "twitter": "@vbmendes",
+            "github": "http://github.com/vbmendes",
+        },
+    ],
+}
 
 
 # -- Extension configuration -------------------------------------------------
